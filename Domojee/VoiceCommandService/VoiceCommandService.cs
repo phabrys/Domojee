cd021b1ffaeb4b240bf8f652d97311d8ab2fee66
halbout michael <michaelhalbout@outlook.fr>
2016-01-29 06:15:27 +0100

    Ajout d'un arriere plan Cortana

---
+++ Domojee/VoiceCommandService/VoiceCommandService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Background;
+
+namespace Domojee.VoiceCommands
+{
+    public sealed class DomojeeVoiceCommandService : IBackgroundTask
+    {
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            BackgroundTaskDeferral _deferral = taskInstance.GetDeferral();
+
+            //
+            // TODO: Insert code 
+            //
+            //
+
+            _deferral.Complete();
+        }
+    }
+}
