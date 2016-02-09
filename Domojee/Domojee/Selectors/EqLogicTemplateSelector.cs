6a378506f767b1776b1fc6af4c9a72c2e65b41cf
Julien SIMON <simonj@hotmail.fr>
2016-02-09 09:56:52 +0100

    Modification TemplateSelector

--- Domojee/Domojee/Selectors/EqLogicTemplateSelector.cs
+++ Domojee/Domojee/Selectors/EqLogicTemplateSelector.cs
@@ -9,19 +9,37 @@ using Windows.UI.Xaml.Controls;
 
 namespace Domojee.Selectors
 {
-    class EqLogicTemplateSelector : DataTemplateSelector
+    internal class EqLogicTemplateSelector : DataTemplateSelector
     {
         public DataTemplate ZWaveEqLogicTemplate { get; set; }
+        public DataTemplate SonosEqLogicTemplate { get; set; }
+        public DataTemplate OnOffEqLogicTemplate { get; set; }
         public DataTemplate EqLogicTemplate { get; set; }
 
         protected override DataTemplate SelectTemplateCore(object item, DependencyObject container)
         {
             var eq = item as EqLogic;
             var element = container as FrameworkElement;
+            if (eq.display != null)
+                if (eq.display.customParameters != null)
+                    if (eq.display.customParameters.DomojeeTemplate != null)
+                        switch (eq.display.customParameters.DomojeeTemplate.ToLower())
+                        {
+                            case "sonos":
+                                return SonosEqLogicTemplate;
+
+                            case "onoff":
+                                return OnOffEqLogicTemplate;
+
+                            default:
+                                return EqLogicTemplate;
+                        }
+
             switch (eq.eqType_name)
             {
                 case "openzwave":
                     return ZWaveEqLogicTemplate;
+
                 default:
                     return EqLogicTemplate;
             }
