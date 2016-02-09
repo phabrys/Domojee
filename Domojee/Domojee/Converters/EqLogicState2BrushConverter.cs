c52cad3c2cd7c32e42dd71b146855ab38b2d2a30
Julien SIMON <simonj@hotmail.fr>
2016-02-09 09:18:36 +0100

    Ajout de la condition "on" pour le converter state vers brush

--- Domojee/Domojee/Converters/EqLogicState2BrushConverter.cs
+++ Domojee/Domojee/Converters/EqLogicState2BrushConverter.cs
@@ -1,30 +1,43 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Windows.UI;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Media;
 
 namespace Domojee.Converters
 {
-    class EqLogicState2BrushConverter : IValueConverter
+    internal class EqLogicState2BrushConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, string language)
         {
             var state = value as string;
             var brush = new SolidColorBrush();
-            if (state == "1")
-                brush.Color = Colors.LightGreen;
+            if (state == "1" || state.ToLower() == "on")
+                brush = GetColorFromHexa("#8CC152");
             else
-                brush.Color = Colors.LightCoral;
+                brush = GetColorFromHexa("#434A54");
             return brush;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, string language)
         {
             throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Convertie une couleur hexa
+        /// </summary>
+        /// <param name="hexaColor"></param>
+        /// <returns></returns>
+        public static SolidColorBrush GetColorFromHexa(string hexaColor)
+        {
+            return new SolidColorBrush(
+                    Color.FromArgb(
+                        255,
+                        System.Convert.ToByte(hexaColor.Substring(1, 2), 16),
+                        System.Convert.ToByte(hexaColor.Substring(3, 2), 16),
+                        System.Convert.ToByte(hexaColor.Substring(5, 2), 16)
+                )
+            );
         }
     }
 }
