672cc887421213d5d19a9a3a035b99e84be93a38
Julien SIMON <simonj@hotmail.fr>
2016-02-09 09:51:19 +0100

    Ajout d'une classe pour appliquer plusieurs converters en XAML

---
+++ Domojee/Domojee/Converters/ValueConverterGroup.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Windows.UI.Xaml.Data;
+
+namespace Domojee.Converters
+{
+    internal class ValueConverterGroup : List<IValueConverter>, IValueConverter
+    {
+        #region Public Methods
+
+        public object Convert(object value, Type targetType, object parameter, string culture)
+        {
+            return this.Aggregate(value, (current, converter) => converter.Convert(current, targetType, parameter, culture));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion Public Methods
+    }
+}
